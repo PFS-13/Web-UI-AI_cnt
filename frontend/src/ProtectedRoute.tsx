d94ff5ea36d8bfdabb286e9c7d6ce708230dcdba
Mochamad Fathur Rabbani <fathur@cnt.id>
2025-10-07 09:51:03 +0700

    [Fix] protected route

--- frontend/src/ProtectedRoute.tsx
+++ frontend/src/ProtectedRoute.tsx
@@ -29,7 +29,7 @@ export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   }, [id]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return null;
   }
 
   if (!user) {
