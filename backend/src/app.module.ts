354ed4b5518669f5a90f4ab54efd39fbcfb01846
Mochamad Fathur Rabbani <fathur@cnt.id>
2025-10-15 10:42:21 +0700

    [Fix] app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,12 +13,12 @@ import { AttachedMessageModule } from './modules/attached.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      // url : process.env.DATABASE_URL,
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT ?? '5432'),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      url : process.env.DATABASE_URL,
+      // host: process.env.POSTGRES_HOST,
+      // port: parseInt(process.env.POSTGRES_PORT ?? '5432'),
+      // username: process.env.POSTGRES_USER,
+      // password: process.env.POSTGRES_PASSWORD,
+      // database: process.env.POSTGRES_DB,
       synchronize: false,
       autoLoadEntities: true,
       ssl: process.env.NODE_ENV === 'production' ? {
